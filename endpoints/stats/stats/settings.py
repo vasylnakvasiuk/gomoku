0dae61fc4e7c63b1fda5ca1ab1d3aae1288154b9
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2013-07-30 20:53:16 +0300

    add static and media path

--- endpoints/stats/stats/settings.py
+++ endpoints/stats/stats/settings.py
@@ -55,22 +55,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = rel('..', '..', '..', '..', 'media', 'stats')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/stats/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = rel('..', '..', '..', '..', 'static', 'stats')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/stats/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
